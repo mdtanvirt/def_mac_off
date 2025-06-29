6e8b853816b4dca3631554fb4a938b0081c063b4
Tanvir Rahman <mdtanvirt@gmail.com>
2025-06-29 17:15:30 +0600

    Update

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -19,12 +19,19 @@ class ProductSerializer(serializers.ModelSerializer):
         return value
 
 class OrderItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
     class Meta:
         model = OrderItem
-        fields = ('id', 'product', 'quantity')    
+        fields = ('id', 'product', 'quantity', 'order')    
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField(method_name='total')
+
+    def total(self, obj):
+        order_items = obj.items.all()
+        return sum(order_item.sub_total for order_item in order_items)
+
     class Meta:
         model = Order
-        fields = ('order_id', 'created_at', 'user', 'status', 'items')
+        fields = ('order_id', 'created_at', 'user', 'status', 'items', 'total_price')
